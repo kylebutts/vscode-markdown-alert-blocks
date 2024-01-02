a2f868762bdd9328975d77fd4b70bb01d140c4af
Kyle Butts <buttskyle96@gmail.com>
2024-01-01 20:54:43 -0500

    Switch to `import` instead of `require`

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
-const { alert } = require("@mdit/plugin-alert");
+import { alert } from '@mdit/plugin-alert'
 
 export function activate(context: vscode.ExtensionContext) {
     const config = vscode.workspace.getConfiguration('markdown-alert');
     const isEnabled = config.get<boolean>('markdown-alert.enabled', true);
     const isDeepEnabled = config.get<boolean>('markdown-alert.deep-enabled', true);
-
 
     return {
         extendMarkdownIt(md: any) {
