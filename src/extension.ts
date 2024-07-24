8e5242ad6ce2499b0fd80ffccbf61d3eedca95d0
Rafi Strauss <rafi.strauss@nbcuni.com>
2024-07-24 14:20:59 -0400

    feat: Add commands for inserting markdown alert blocks

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,31 @@ export function activate(context: vscode.ExtensionContext) {
     const isEnabled = config.get<boolean>('markdown-alert.enabled', true);
     const isDeepEnabled = config.get<boolean>('markdown-alert.deep-enabled', true);
 
+    let disposable = vscode.commands.registerCommand('markdownAlert.insertNote', function () {
+        insertAlertBlock("> [!NOTE]\n> ");
+      });
+      context.subscriptions.push(disposable);
+
+    let insertTipDisposable = vscode.commands.registerCommand('markdownAlert.insertTip', function () {
+        insertAlertBlock("> [!TIP]\n> ");
+    });
+    context.subscriptions.push(insertTipDisposable);
+
+    let insertImportantDisposable = vscode.commands.registerCommand('markdownAlert.insertImportant', function () {
+        insertAlertBlock("> [!IMPORTANT]\n> ");
+    });
+    context.subscriptions.push(insertImportantDisposable);
+
+    let insertWarningDisposable = vscode.commands.registerCommand('markdownAlert.insertWarning', function () {
+        insertAlertBlock("> [!WARNING]\n> ");
+    });
+    context.subscriptions.push(insertWarningDisposable);
+
+    let insertCautionDisposable = vscode.commands.registerCommand('markdownAlert.insertCaution', function () {
+        insertAlertBlock("> [!CAUTION]\n> ");
+    });
+    context.subscriptions.push(insertCautionDisposable);
+
     return {
         extendMarkdownIt(md: any) {
             if (!isEnabled) {
@@ -15,3 +40,17 @@ export function activate(context: vscode.ExtensionContext) {
         }
     }
 }
+
+function insertAlertBlock(text: string) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.showErrorMessage("No active editor found");
+      return;
+    }
+  
+    const selection = editor.selection;
+    const range = new vscode.Range(selection.start, selection.end);
+    editor.edit((editBuilder) => {
+      editBuilder.replace(range, text);
+    });
+  }
